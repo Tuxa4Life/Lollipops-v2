6cd1c2b5df539141ef669997f706acfe49d4ee1e
Tuxa4Life <nikoloztuxa@gmail.com>
2023-07-27 17:33:27 +0400

    React 18

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
-import ReactDom from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import App from './App';
 
-ReactDom.render(<App/>, document.querySelector('#root'))
+const el = document.querySelector('#root')
+const root = ReactDOM.createRoot(el)
+
+root.render(<App />)
