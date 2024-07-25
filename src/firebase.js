ba3b8fde03220a0dffbb4e0ea44d6c8466343cd7
Nikoloz Tukhashvili <66478283+Tuxa4Life@users.noreply.github.com>
2024-07-25 21:22:47 +0400

    API Data Hidden

--- src/firebase.js
+++ src/firebase.js
@@ -2,14 +2,7 @@ import { initializeApp } from 'firebase/app'
 import { getFirestore } from '@firebase/firestore' 
 import { getStorage } from 'firebase/storage'
 
-const firebaseConfig = {
-    apiKey: "AIzaSyBHc3YKk9nghuOTs1PSggOQ9Fbt4YJRVb8",
-    authDomain: "lollipops-rustavi.firebaseapp.com",
-    projectId: "lollipops-rustavi",
-    storageBucket: "lollipops-rustavi.appspot.com",
-    messagingSenderId: "568260549407",
-    appId: "1:568260549407:web:7e080255435d86165cae2a"
-  };
+const firebaseConfig = {};
 const app = initializeApp(firebaseConfig)
 
 export const db = getFirestore(app)
